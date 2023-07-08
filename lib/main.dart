264fffb1d434af960af87ee6d448c48ff41f5d0d
omer-bronix <omer@bronixengineering.com>
2023-07-08 22:41:07 +0300

    get_storage added, storage services added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_getx_todoapp/app/data/services/storage/services.dart';
 import 'package:flutter_getx_todoapp/app/modules/home/view.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
+void main() async {
+  await GetStorage.init();
+  await Get.putAsync(() => StorageService().init());
   runApp(const MyApp());
 }
 
