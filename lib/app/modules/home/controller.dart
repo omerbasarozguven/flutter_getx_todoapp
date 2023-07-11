754a9313ccef676d1e57e072036dc782a137b749
omer-bronix <omer@bronixengineering.com>
2023-07-11 17:54:50 +0300

    tutorial part 2 completed

--- lib/app/modules/home/controller.dart
+++ lib/app/modules/home/controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_getx_todoapp/app/data/models/task.dart';
 import 'package:flutter_getx_todoapp/app/data/services/storage/repository.dart';
 import 'package:get/get.dart';
@@ -7,11 +8,31 @@ class HomeController extends GetxController {
   HomeController({required this.taskRepository});
 
   final tasks = <Task>[].obs;
+  final formKey = GlobalKey<FormState>();
+  final editCtrl = TextEditingController();
+  final chipIndex = 0.obs;
 
   @override
   void onInit() {
     super.onInit();
     tasks.assignAll(taskRepository.readTasks());
     ever(tasks, (_) => taskRepository.writeTasks(tasks));
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+
+  void changeChipIndex(int value) {
+    chipIndex.value = value;
+  }
+
+  bool addTask(Task task) {
+    if (tasks.contains(task)) {
+      return false;
+    }
+    tasks.add(task);
+    return true;
   }
 }
--- lib/app/modules/home/view.dart
+++ lib/app/modules/home/view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_getx_todoapp/app/core/utils/extensions.dart';
+import 'package:flutter_getx_todoapp/app/data/models/task.dart';
 import 'package:flutter_getx_todoapp/app/modules/home/controller.dart';
 import 'package:flutter_getx_todoapp/app/modules/home/widgets/add_card.dart';
+import 'package:flutter_getx_todoapp/app/modules/home/widgets/task_card.dart';
 import 'package:get/get.dart';
 
 class HomePage extends GetView<HomeController> {
@@ -18,18 +20,23 @@ class HomePage extends GetView<HomeController> {
               child: Text(
                 'My List',
                 style: TextStyle(
-                  fontSize: 24.0.sp,
+                  fontSize: 4.0.sp,
                   fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-            GridView.count(
-              crossAxisCount: 2,
-              shrinkWrap: true,
-              physics: const ClampingScrollPhysics(),
-              children: [
-                AddCard(),
-              ],
+            Obx(
+              () => GridView.count(
+                crossAxisCount: 2,
+                shrinkWrap: true,
+                physics: const ClampingScrollPhysics(),
+                children: [
+                  ...controller.tasks
+                      .map((element) => TaskCard(task: element))
+                      .toList(),
+                  AddCard(),
+                ],
+              ),
             ),
           ],
         ),
